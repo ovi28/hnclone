6d88436ab7b8a32fc70c2a3e363f381c65e50bd6
UsagiBo <g.e.usagi@gmail.com>
2017-11-17 18:33:51 +0100

    FrontPage

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
 <?php
 include "config.php";
-echo"<a href='login.php' style='text-align:right'>Log in</a>";
-$sql = 'SELECT * FROM post ORDER BY post_id DESC LIMIT 50';
-foreach ($pdo->query($sql) as $row) {
-      print .$row['post_title'].
-      print .$row['post_text'].
-      $sql2 = 'SELECT * FROM vote Where post_id='.$row['post_id']. '';
-      foreach ($pdo->query($sql2) as $row2) {
-            print $karma =$row2['votes_up']-$row2['votes_down'];
-            
-      }
-      
-}
-
  
 ?>

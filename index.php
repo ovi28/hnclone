19c5cb594cb82cc859939218bde16b5c8fc01ec7
UsagiBo <g.e.usagi@gmail.com>
2017-11-17 18:36:54 +0100

    Styled front page

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
 include "config.php";
+echo"<a href='login.php' style='text-align:right'>Log in</a>";
+$sql = 'SELECT * FROM post ORDER BY post_id DESC LIMIT 50';
+foreach ($pdo->query($sql) as $row) {
+      echo "<table style='background-color:beige'>";
+      echo "<tr>";
+      echo "<td style='font-weight:bold'>".$row['post_title']."</td>"; 
+      echo "<tr>";
+      echo "<td style='color:grey'>".$row['post_text']."</td>"; 
+      $sql2 = 'SELECT * FROM vote Where post_id='.$row['post_id']. '';
+      foreach ($pdo->query($sql2) as $row2) {
+            $karma =$row2['votes_up']-$row2['votes_down'];
+            echo "<td style='color:red'>".$karma."</td>";  
+      }
+      echo "<hr>";
+}
+echo "</table>";
  
 ?>
